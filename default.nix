b6c853e48bb6e9f38e639ba27c8051ea9d53ef1d
Shestakov, Aleksandr <a.shestakov@itransition.com>
2021-02-25 00:19:25 +0300

    Removing comments

--- default.nix
+++ default.nix
@@ -4,8 +4,6 @@ let
 in {
   pre-commit-check = nix-pre-commit-hooks.run {
     src = ./.;
-    # If your hooks are intrusive, avoid running on each commit with a default_states like this:
-    # default_stages = ["manual" "push"];
     hooks = {
       vscode = {
         enable = true;
