090fbc84b511e6e999d77eb61d1ebd27cb19aa9c
Shestakov, Aleksandr <a.shestakov@itransition.com>
2021-03-16 09:49:28 +0300

    Migrating neovim to the new configuration framework.

--- home-manager/neovim/default.nix
+++ home-manager/neovim/default.nix
@@ -1,35 +1,112 @@
-{ config, pkgs, ... }: {
-  programs.neovim = {
-    enable = true;
-    viAlias = true;
-    vimAlias = true;
-    withNodeJs = true;
-    vimdiffAlias = true;
-    plugins = with pkgs.vimPlugins; [
-      vim-airline
-      vim-airline-themes
-      ctrlp-vim
-      vim-nix
-      vim-gitgutter
-      vim-startify
-      coc-nvim
-      fzf-vim
+{ config, pkgs, ... }:
+let
 
-      (pkgs.vimUtils.buildVimPluginFrom2Nix {
-        pname = "rigel";
-        version = "2020-08-25";
-        src = pkgs.fetchFromGitHub {
-          owner = "Rigellute";
-          repo = "rigel";
-          rev = "83874d9abd7c5f5df881df75d6c5e41ea26e39de";
-          sha256 = "1rprzhp1vbis5jz464pb4vmaj2r8r62fki7s5ddxh1v0j4i1sb97";
-        };
-        dependencies = [ ];
-      })
-
-    ];
-
-    extraConfig = builtins.readFile ./vimrc;
+  envy = builtins.fetchGit {
+    url = "https://github.com/Shados/envy";
+    ref = "master";
   };
 
+  rigel = (pkgs.vimUtils.buildVimPluginFrom2Nix {
+    pname = "rigel";
+    version = "2020-08-25";
+    src = pkgs.fetchFromGitHub {
+      owner = "Rigellute";
+      repo = "rigel";
+      rev = "83874d9abd7c5f5df881df75d6c5e41ea26e39de";
+      sha256 = "1rprzhp1vbis5jz464pb4vmaj2r8r62fki7s5ddxh1v0j4i1sb97";
+    };
+    dependencies = [ ];
+  });
+
+  issw = (
+
+    pkgs.stdenv.mkDerivation rec {
+      pname = "issw";
+      version = "v0.3";
+
+      src = pkgs.fetchFromGitHub {
+        owner = "vovkasm";
+        repo = "input-source-switcher";
+        rev = version;
+        sha256 = "sha256-JjMNdpqt10CHpUGt5kX15bVDmqm1CbD5lqyLlXY1Jfg=";
+      };
+
+      nativeBuildInputs = [ pkgs.cmake ];
+      buildInputs = [ pkgs.darwin.apple_sdk.frameworks.Carbon ];
+
+    }
+
+  );
+
+  vim-xkbswitch-mac = pkgs.vimPlugins.vim-xkbswitch.overrideAttrs (old: {
+    patchPhase = ''
+      substituteInPlace plugin/xkbswitch.vim --replace /usr/local/lib/libxkbswitch.dylib ${issw}/lib/libInputSourceSwitcher.dylib
+    '';
+    buildInputs = [ issw ];
+  });
+
+in {
+
+  imports = [ (import "${envy}/home-manager.nix" { }) ];
+
+  home.packages = [ pkgs.nix-prefetch-git ];
+  sn.programs.neovim = {
+    extraConfig = builtins.readFile ./vimrc;
+    # mergePlugins = true;
+
+    pluginRegistry = {
+      vim-devicons = {
+        enable = true;
+        after = [
+          "nerdtree"
+          "vim-airline"
+          "ctrlp-vim"
+          "powerline/powerline"
+          "denite-nvim"
+          "unite-vim"
+          "lightline-vim"
+          "vim-startify"
+          "vimfiler"
+          "vim-flagship"
+        ];
+      };
+
+      vim-airline = {
+        enable = true;
+        nvimrc = { postPlugin = "let g:airline_powerline_fonts = 1"; };
+      };
+
+      rigel = {
+        enable = true;
+        source = rigel;
+        nvimrc = {
+          postPlugin = ''
+            set termguicolors
+            syntax enable
+
+            colorscheme rigel
+          '';
+        };
+      };
+
+      vim-xkbswitch-mac = {
+        enable = true;
+        source = vim-xkbswitch-mac;
+        nvimrc = { postPlugin = "let g:XkbSwitchEnabled = 1"; };
+      };
+      ctrlp-vim.enable = true;
+      vim-nix.enable = true;
+      vim-gitgutter.enable = true;
+      vim-startify.enable = true;
+      coc-nvim = {
+        enable = true;
+        binDeps = [ pkgs.nodejs ];
+      };
+      fzf-vim.enable = true;
+      nerdtree.enable = true;
+      haskell-vim.enable = true;
+      kotlin-vim.enable = true;
+      vim-markdown.enable = true;
+    };
+  };
 }
