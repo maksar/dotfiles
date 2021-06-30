f8faf6b9ce6a2e7421635b9222adb37dd8cb8abb
Shestakov, Aleksandr <a.shestakov@itransition.com>
2021-06-30 14:56:10 +0300

    Adding nixbuild.net

--- darwin/nix/default.nix
+++ darwin/nix/default.nix
@@ -10,7 +10,7 @@
     "$HOME/.nix-defexpr/channels"
   ];
   nix.trustedUsers = builtins.attrNames config.users.users;
-  # nix.gc.automatic = true;
+  nix.gc.automatic = false;
   nix.extraOptions = ''
     gc-keep-derivations = true
     gc-keep-outputs = true
@@ -19,15 +19,25 @@
   nix.buildCores = 6;
   nix.maxJobs = 6;
   nix.distributedBuilds = true;
-  nix.buildMachines = [{
-    hostName = "nix-docker";
-    sshUser = "root";
-    sshKey = "/etc/nix/docker_rsa";
-    systems = [ "x86_64-linux" ];
-    supportedFeatures = [ "big-parallel" ];
-    maxJobs = 6;
-    buildCores = 6;
-  }];
+  nix.buildMachines = [
+    {
+      hostName = "nix-docker";
+      sshUser = "root";
+      sshKey = "/etc/nix/docker_rsa";
+      systems = [ "x86_64-linux" ];
+      supportedFeatures = [ "big-parallel" ];
+      maxJobs = 6;
+      buildCores = 6;
+    }
+    {
+      hostName = "eu.nixbuild.net";
+      sshUser = "root";
+      sshKey = "/etc/nix/nixbuild.net";
+      systems = [ "x86_64-linux" ];
+      supportedFeatures = [ "big-parallel" "benchmark" ];
+      maxJobs = 100;
+    }
+  ];
   users.nix.configureBuildUsers = true;
 
   nix.binaryCachePublicKeys =
