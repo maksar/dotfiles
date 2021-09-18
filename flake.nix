bdabc04c9adada74c8f9219d47dc02f32dea514e
Shestakov Alex <Maksar.mail@gmail.com>
2021-09-18 16:57:57 +0300

    linux

--- flake.nix
+++ flake.nix
@@ -112,6 +112,18 @@
       };
     };
 
+    # Build and activate with `nix build .#cloudVM.activationPackage; ./result/activate`
+    cloudVM = home-manager.lib.homeManagerConfiguration {
+      system = "x86_64-linux";
+      stateVersion = "21.05";
+      homeDirectory = "/home/maksar";
+      username = "maksar";
+      configuration = {
+        imports = [ homeManagerCommonConfig ];
+        nixpkgs = nixpkgsConfig;
+      };
+    };
+
     overlays = with inputs; [
       (
         final: prev: {
